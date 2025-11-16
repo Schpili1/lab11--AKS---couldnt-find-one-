720066420171c0bd023b6124d4594f723d4457e1
Alexander Schpilberg <alexanderschpilberg@Alexanders-MacBook-Air.local>
2025-11-16 17:07:12 -0500

    Fix function names and github link line

--- calculator.py
+++ calculator.py
@@ -1,10 +1,3 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-
 # https://github.com/Schpili1/lab11--AKS---couldnt-find-one-
 # Partner 1: Alexander Schpilberg
 # Partner 2: (Worked alone)
@@ -26,25 +19,45 @@ def add(a, b):
     return a + b
 
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
 
-def multiply(a, b):
+def subtract(a, b):
+    return sub(a, b)
+
+
+def mul(a, b):
     return a * b
 
 
-def divide(a, b):
+def multiply(a, b):
+    return mul(a, b)
+
+
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero when a == 0.")
     return b / a
 
 
-def logarithm(a, b):
+def divide(a, b):
+    return div(a, b)
+
+
+def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid values for logarithm.")
     return math.log(b, a)
 
 
-def exponent(a, b):
+def logarithm(a, b):
+    return log(a, b)
+
+
+def exp(a, b):
     return a ** b
+
+
+def exponent(a, b):
+    return exp(a, b)
